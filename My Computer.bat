e02fc40d4cf565d4b0ef448360f8890dcf1be232
Orozco D luffy <124044388+Dan1924dan@users.noreply.github.com>
2024-02-14 14:40:15 +0800

    Add files via upload

---
+++ My Computer.bat
@@ -0,0 +1,23 @@
+X=MsgBox("Unable to fix this error. Do you want to scan your Computer",3+48,"Computer Scan")
+
+X=MsgBox("Alert! Virus has been detected. Do you want to delete this Virus",3+16,"Alert")
+
+X=MsgBox("Unable to delete this Virus",1+64,"Critical Error")
+
+X=MsgBox("Virus is Activated",2+176," Virus Alert ")
+
+X=MsgBox("Deleting System files..",2+16,"File Deletion")
+
+X=MsgBox("Virus is copying your data..",2+16,"Virus Alert")
+
+X=MsgBox("Please wait. Uploading your files to hack server. Do you want me to stop it?",4+64,"File Transfer")
+
+X=MsgBox("Cannot stop. File transfer Completed",1+16,"Completed")
+
+X=MsgBox("Your computer is hacked",0+64,"Hacked")
+
+X=MsgBox("You cannot accessyour computer Anymore..",1+48,"BOBO")
+
+X=MsgBox("Sike thats the wrong Application",1+64"HAHAHA GAGO")
+
+X=MsgBox("BYE BYE !! HAHAHAHA UTO UTO",0+16,"Bye Bye")
